5481bc8eb7e0773422baba81554cd54a318218a0
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-25 21:34:07 +0530

    mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
+import { useQuery, useMutation } from "@apollo/client/react";
 import "./App.css";
 import { useState } from "react";
 
@@ -27,10 +27,28 @@ const GET_POSTS = gql`
 		}
 	}
 `;
+
+const CREATE_POST = gql`
+	mutation ($input: CreatePostInput!) {
+		createPost(input: $input) {
+			id
+			title
+			body
+		}
+	}
+`;
 const App: React.FC = () => {
-	const { loading, error, data } = useQuery<GetPostsData>(GET_POSTS);
+	const { loading, error, data, refetch } = useQuery<GetPostsData>(GET_POSTS);
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
+
+	const [createPost, { loading: creating }] = useMutation(CREATE_POST, {
+		onCompleted: () => {
+			refetch();
+			setTitle("");
+			setBody("");
+		},
+	});
 
 	console.log("data", data);
 
@@ -42,7 +60,7 @@ const App: React.FC = () => {
 			<div className='p-4 max-w-xl mx-auto'>
 				<h2 className='text-2xl font-bold mb-4'>Posts</h2>
 				<ul className='space-y-3 mb-6'>
-					{data?.posts.data.map((post: any) => (
+					{data?.posts.data.map((post: Post) => (
 						<li
 							key={post.id}
 							className='border p-3 rounded shadow-sm'>
@@ -51,6 +69,34 @@ const App: React.FC = () => {
 						</li>
 					))}
 				</ul>
+				<h2>Create Post</h2>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						createPost({ variables: { input: { title, body } } });
+					}}>
+					<input
+						value={title}
+						placeholder='Title'
+						onChange={(e) => {
+							setTitle(e.target.value);
+						}}
+						required
+					/>
+					<textarea
+						placeholder='Body'
+						value={body}
+						onChange={(e) => setBody(e.target.value)}
+						className='border p-2 w-full rounded'
+						rows={3}
+						required
+					/>
+					<button
+						type='submit'
+						disabled={creating}>
+						{creating ? "Creating..." : "Create Post"}
+					</button>
+				</form>
 			</div>
 		</>
 	);
