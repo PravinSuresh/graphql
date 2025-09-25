519af80883ed411659406b6ee4ffdafcccc4cbe0
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-25 21:18:28 +0530

    fetch posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { gql } from "@apollo/client";
-import { useQuery, useMutation } from "@apollo/client/react";
+import { useQuery } from "@apollo/client/react";
 import "./App.css";
-import { useState, type JSX } from "react";
+import { useState } from "react";
+
+// 1. Define TS type for the query result:
+interface Post {
+	id: string;
+	title: string;
+	body: string;
+}
+
+interface GetPostsData {
+	posts: {
+		data: Post[];
+	};
+}
 
 const GET_POSTS = gql`
 	query {
@@ -14,18 +27,33 @@ const GET_POSTS = gql`
 		}
 	}
 `;
-function App(): JSX.Element {
-	const { loading, error, data, refetch } = useQuery(GET_POSTS);
+const App: React.FC = () => {
+	const { loading, error, data } = useQuery<GetPostsData>(GET_POSTS);
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
+
+	console.log("data", data);
 
 	if (loading) return <p>Loading posts...</p>;
 	if (error) return <p>Error: {error.message}</p>;
 	return (
 		<>
 			<h1>Learn GraphQL</h1>
+			<div className='p-4 max-w-xl mx-auto'>
+				<h2 className='text-2xl font-bold mb-4'>Posts</h2>
+				<ul className='space-y-3 mb-6'>
+					{data?.posts.data.map((post: any) => (
+						<li
+							key={post.id}
+							className='border p-3 rounded shadow-sm'>
+							<h2 className='font-semibold'>{post.title}</h2>
+							<p className='text-sm text-gray-600'>{post.body}</p>
+						</li>
+					))}
+				</ul>
+			</div>
 		</>
 	);
-}
+};
 
 export default App;
