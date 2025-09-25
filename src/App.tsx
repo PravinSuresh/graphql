41bc5394d09c8f89b15ceb7526d65e84922b448c
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-25 21:04:26 +0530

    initial apollo client and graphql set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client/react";
+import "./App.css";
+import { useState, type JSX } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_POSTS = gql`
+	query {
+		posts(options: { paginate: { page: 1, limit: 5 } }) {
+			data {
+				id
+				title
+				body
+			}
+		}
+	}
+`;
+function App(): JSX.Element {
+	const { loading, error, data, refetch } = useQuery(GET_POSTS);
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	if (loading) return <p>Loading posts...</p>;
+	if (error) return <p>Error: {error.message}</p>;
+	return (
+		<>
+			<h1>Learn GraphQL</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
